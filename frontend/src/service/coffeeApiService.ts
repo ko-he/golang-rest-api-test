fe0c70562cf7ead8e1e0c59b4a74395840bbf91f
ko-he <localhost.ko@gmail.com>
2025-01-31 13:00:15 +0900

    commit

--- frontend/src/service/coffeeApiService.ts
+++ frontend/src/service/coffeeApiService.ts
@@ -1,28 +1,36 @@
 import { request } from "./apiClient"
 
-export const createCoffee = async (): Promise<string> => {
+export const createCoffee = async (
+			name: string,
+			comment: string,
+			productionArea: string,
+			richScore: number,
+			bitternessScore: number,
+			sournessScore: number,
+			aromaScore: number,
+): Promise<string> => {
 	const response = await request({
 		url: '/api/v1/coffees',
 		method: 'post',
 		data: {
-			name: "",
-			comment: "",
-			productionArea: "",
-			richScore: 0,
-			bitternessScore: 0,
-			sournessScore: 0,
-			aromaScore: 0
+			name,
+			comment,
+			productionArea,
+			richScore,
+			bitternessScore,
+			sournessScore,
+			aromaScore,
 		}
 	})
 
 	return response.data.id
 }
 
-export const getCoffee = async (): Promise<string> => {
+export const getCoffee = async (id: string): Promise<string> => {
 	const response = await request({
 		url: '/api/v1/coffees/{id}',
 		method: 'get',
-		pathParam: { id: "dummy-coffee-uuid" }
+		pathParam: { id }
 	})
 
 	return response.data.coffee.id
