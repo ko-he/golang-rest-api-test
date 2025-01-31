94286c61deaafafda3f021924fd4f5f40f616e62
ko-he <localhost.ko@gmail.com>
2025-01-31 13:05:33 +0900

    indent

--- frontend/src/service/coffeeApiService.ts
+++ frontend/src/service/coffeeApiService.ts
@@ -1,37 +1,37 @@
 import { request } from "./apiClient"
 
 export const createCoffee = async (
-	name: string,
-	comment: string,
-	productionArea: string,
-	richScore: number,
-	bitternessScore: number,
-	sournessScore: number,
-	aromaScore: number,
+  name: string,
+  comment: string,
+  productionArea: string,
+  richScore: number,
+  bitternessScore: number,
+  sournessScore: number,
+  aromaScore: number,
 ): Promise<string> => {
-	const response = await request({
-		url: '/api/v1/coffees',
-		method: 'post',
-		data: {
-			name,
-			comment,
-			productionArea,
-			richScore,
-			bitternessScore,
-			sournessScore,
-			aromaScore,
-		}
-	})
+  const response = await request({
+    url: '/api/v1/coffees',
+    method: 'post',
+    data: {
+      name,
+      comment,
+      productionArea,
+      richScore,
+      bitternessScore,
+      sournessScore,
+      aromaScore,
+    }
+  })
 
-	return response.data.id
+  return response.data.id
 }
 
 export const getCoffee = async (id: string): Promise<string> => {
-	const response = await request({
-		url: '/api/v1/coffees/{id}',
-		method: 'get',
-		pathParam: { id }
-	})
+  const response = await request({
+    url: '/api/v1/coffees/{id}',
+    method: 'get',
+    pathParam: { id }
+  })
 
-	return response.data.coffee.id
+  return response.data.coffee.id
 }
